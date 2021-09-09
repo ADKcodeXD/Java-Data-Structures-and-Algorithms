48a1085bd5efc9d8a6832521e11abea734dc7f17
ADKcode <790532173@qq.com>
2021-09-09 17:50:09 +0800

    是否回文串

---
+++ src/com/adk/leetcode/IsPalindrome.java
@@ -0,0 +1,29 @@
+package com.adk.leetcode;
+
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        if(s.length()==0) return true;
+        int left=0;
+        int right=s.length()-1;
+        while (left<right){
+            while (left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while (left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }else {
+                right--;left++;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s="dsaihnfiaslfd";
+        boolean palindrome = new IsPalindrome().isPalindrome(s);
+        System.out.println(palindrome);
+    }
+}
